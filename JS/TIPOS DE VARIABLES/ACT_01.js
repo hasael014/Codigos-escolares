27beba334ac2ae46a02811fb7fdf766a3b40f7b2
hasael014 <79614326+hasael014@users.noreply.github.com>
2021-04-29 00:22:12 -0500

    code

---
+++ JS/TIPOS DE VARIABLES/ACT_01.js
@@ -0,0 +1,17 @@
+var fecha = new Date()
+
+var hora = fecha.getHours()
+var minutos = fecha.getMinutes()
+var segundos = fecha.getSeconds()
+
+if (hora < 10) {
+    hora = "0" + hora
+}
+if (minutos < 10) {
+    minutos = "0" + minutos
+}
+if (segundos < 10) {
+    segundos = "0" + segundos
+}
+
+document.write("Son las " + hora + ":" + minutos + ":" + segundos)
